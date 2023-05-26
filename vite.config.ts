89d16d2ac6329c3caae4cffe5bb351e36d210f20
wangyuan <wangyuan@qianlima.com>
2023-05-26 15:14:32 +0800

    feat: 设置@快捷指向

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D 开发者可以在 TypeScript 中使用 Node.js 的 API
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置`@`指向`src`目录
+    }
+  },
+  base: './', // 设置打包路径
 })
