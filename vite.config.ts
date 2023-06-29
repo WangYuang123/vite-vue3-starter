1e0262c6004355e8e0a68bf18d79d388f3e88a60
wangyuan <wangyuan@qianlima.com>
2023-06-29 19:05:54 +0800

    feat: 整合axios

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { UserConfig, ConfigEnv, loadEnv, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D 开发者可以在 TypeScript 中使用 Node.js 的 API
 import { resolve } from "path";
@@ -13,81 +13,84 @@ import IconsResolver from "unplugin-icons/resolver";
 
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
-import UnoCSS from 'unocss/vite'
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ["vue"],
-      eslintrc: {
-        enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
-        filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        eslintrc: {
+          enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
+          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+        },
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        vueTemplate: true, // 是否在 vue 模板中自动导入
+        dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 自动导入组件类型声明文件位置，默认根目录
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"], // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          }),
+        ],
+        dts: path.resolve(pathSrc, "types", "components.d.ts"), //  自动导入组件类型声明文件位置，默认根目录
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+      UnoCSS(),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"), // 设置`@`指向`src`目录
       },
-      resolvers: [
-        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-        ElementPlusResolver(),
-        // 自动导入图标组件
-        IconsResolver({}),
-      ],
-      vueTemplate: true, // 是否在 vue 模板中自动导入
-      dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 自动导入组件类型声明文件位置，默认根目录
-    }),
-    Components({
-      resolvers: [
-        // 自动导入 Element Plus 组件
-        ElementPlusResolver(),
-        // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ["ep"], // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
-        }),
-      ],
-      dts: path.resolve(pathSrc, "types", "components.d.ts"), //  自动导入组件类型声明文件位置，默认根目录
-    }),
-    Icons({
-      // 自动安装图标库
-      autoInstall: true,
-    }),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-    UnoCSS()
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"), // 设置`@`指向`src`目录
     },
-  },
-  base: "./", // 设置打包路径
-  server: {
-    port: 4000, // 设置服务启动端口号
-    open: true, // 设置服务启动时是否自动打开浏览器
-    cors: true, // 允许跨域
+    base: "./", // 设置打包路径
+    server: {
+      port: Number(env.VITE_APP_PORT), // 设置服务启动端口号
+      open: true, // 设置服务启动时是否自动打开浏览器
+      cors: true, // 允许跨域
 
-    // 设置代理，根据我们项目实际情况配置
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://xxx.xxx.xxx.xxx:8000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     rewrite: (path) => path.replace('/api/', '/')
-    //   }
-    // }
-  },
-  // vite.config.ts
-  css: {
-    // CSS 预处理器
-    preprocessorOptions: {
-      //define global scss variable
-      scss: {
-        javascriptEnabled: true,
-        additionalData: `@use "@/styles/variables.scss" as *;`,
+      // 设置代理，根据我们项目实际情况配置
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: "http://vapi.youlai.tech", // 线上接口地址
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""), // 替换 /dev-api 为 target 接口地址
+        },
       },
     },
-  },
+    // vite.config.ts
+    css: {
+      // CSS 预处理器
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@use "@/styles/variables.scss" as *;`,
+        },
+      },
+    },
+  };
 });
